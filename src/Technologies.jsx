a42b93fadad1015b2ec1499418718fd905a44a7c
Jose Sierra <47508538+zoddDev@users.noreply.github.com>
2023-04-25 18:05:35 +0200

    Update Technologies.jsx

--- src/Technologies.jsx
+++ src/Technologies.jsx
@@ -284,7 +284,7 @@ export const Technologies = () => {
                           <div className="avatar">
                             <div className="w-12 h-12 mask mask-square">
                               <img
-                                src="https://camo.githubusercontent.com/a16c8cfda098dd31048586344a6555114d5480342b656d866c3a5a8b7698da3d/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f612f61352f417263686c696e75782d69636f6e2d6372797374616c2d36342e7376672f3132303070782d417263686c696e75782d69636f6e2d6372797374616c2d36342e7376672e706e67"
+                                src="https://wiki.installgentoo.com/images/f/f9/Arch-linux-logo.png"
                                 alt="Arch Linux"
                                 className="p-1"
                               />
